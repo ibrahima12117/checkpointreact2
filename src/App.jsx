a89858d75eff1e5eacac4375ddcd68f7580e17f0
IBRAHIMA GUEYE <iboug4337@gmail.com>
2024-10-24 23:35:06 +0000

    checkpointreact2

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <button className="btn">Hello world</button>
+    </div>
+  );
+}
+/*
+export default function (){
+ return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <button className="btn">Hello world</button>
+    </div>
+  )
+};*/
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/App.jsx/app.{html,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
